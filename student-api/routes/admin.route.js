e6d36a21d7d2a622faf79a2e89ba4d8a969525df
thamidu-peiris <thamidu2c@gmail.com>
2026-08-31 16:11:13 +0530

    Refresh Token Added

--- student-api/routes/admin.route.js
+++ student-api/routes/admin.route.js
@@ -7,4 +7,13 @@ import adminLoginSchema from "../middlewares/validation/adminlogin.schema.js";
 const AdminRoute = express.Router();
 AdminRoute.post("/login", validate(adminLoginSchema), AdminLogin);
 
+AdminRoute.post("/refresh-token", (req, res) => {
+    return res.json({
+        message: "token refresh",
+        cookie:req.cookies
+    })
+});
+
+
+
 export default AdminRoute;
